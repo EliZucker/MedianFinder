f6bce317a99a6518af60db90e0589f81f3eac46b
Eli Zucker <github@redytedy.33mail.com>
2015-11-30 12:06:55 -0800

    progress

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+	public static void main(String args[]) {
+		
+	}
+	public static double median(double[] originalList) {
+		
+	}
+	public static double partition(int list[], int desiredIndex) {
+		
+		if(list.length == 1) {
+			return list[0];
+		}
+		
+		for(int i = 1; i < )
+		
+		return 
+	}
+}
